fa3647163d4628ec1427ebb3bda9d2092cffe26a
Eric Montes de Oca <eric.mdoj8@gmail.com>
2021-05-11 19:05:12 -0500

    Force https

--- html/app/Providers/AppServiceProvider.php
+++ html/app/Providers/AppServiceProvider.php
@@ -26,9 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if( env('FORCE_HTTPS', false) ) {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
 
         Schema::defaultStringLength(191); //NEW: Increase StringLength
 
